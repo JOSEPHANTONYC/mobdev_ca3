8c23e88a2c4ce9b11686d832d7640f207f21355c
JOSEPHANTONYC <jantoniocasascalderon@gmail.com>
2022-11-11 15:49:46 +0000

    creating tabs

---
+++ mobdev_ca3/src/app/pages/tab/tab-routing.module.ts
@@ -0,0 +1,58 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabPage } from './tab.page';
+
+const routes: Routes = [
+  {
+    path: 'tab',
+    component: TabPage,
+    children: [
+  
+  {
+    path: 'characters',
+    children: [
+     { path: '',
+    loadChildren: () => import('../characters/characters.module').then( m => m.CharactersPageModule)
+  }
+]
+  },
+ 
+  {
+    path: 'episodes',
+    children: [
+      { path: '',
+    loadChildren: () => import('../episodes/episodes.module').then( m => m.EpisodesPageModule)
+  }
+]
+  },
+  {
+    path: 'quotes',
+    children: [
+      { path: '',
+    loadChildren: () => import('../quotes/quotes.module').then( m => m.QuotesPageModule)
+  }
+]
+  },
+  
+  {
+    path: 'death-count',
+    children: [
+      { path: '',
+    loadChildren: () => import('../death-count/death-count.module').then( m => m.DeathCountPageModule)
+  }
+]
+  },
+  {
+    path: 'tab',
+    children: [
+      { path: '',
+    loadChildren: () => import('./pages/tab/tab.module').then( m => m.TabPageModule)
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabPageRoutingModule {}
